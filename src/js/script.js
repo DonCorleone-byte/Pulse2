f4955861c319030cf6ba630a87b00f9b27b6030b
Maxim <maks_umov1996@mail.ru>
2022-10-09 22:35:44 +0600

    маска ввода номера телефона в форме

--- src/js/script.js
+++ src/js/script.js
@@ -52,3 +52,5 @@ document.querySelector('.next').addEventListener('click', function () {
             $('.overlay, #order').fadeIn('slow');
         });
     });
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
